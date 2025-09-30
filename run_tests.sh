6495576d267d17198314510aed55193e556dcf77
aurianer <aurianer@cscs.ch>
2025-09-30 12:01:36 +0200

    Add execution scripts for beverin images

---
+++ run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eux
+
+proj_dir=~/projects/amd-gpu-benchmarks
+build_dir=$proj_dir/rocPRISM/build
+mkdir -p $build_dir && cd $build_dir
+
+CC=mpicc CXX=mpicxx cmake -G Ninja -DCMAKE_HIP_ARCHITECTURES="gfx90a;gfx942" ..
+ninja
